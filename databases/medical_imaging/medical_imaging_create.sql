66a092a20bc145af99edd0c2d577d322d0b5b9f8
Roberto <romogo17@gmail.com>
2018-10-12 14:57:12 -0600

    Added docs, more test data to med_img and updated README

--- databases/medical_imaging/medical_imaging_create.sql
+++ databases/medical_imaging/medical_imaging_create.sql
@@ -1,4 +1,4 @@
--- Last modification date: 2018-09-14 18:19:12.158
+-- Last modification date: 2018-09-18 00:56:13.434
 
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 DROP SCHEMA IF EXISTS med_img CASCADE;
@@ -9,19 +9,23 @@ CREATE SCHEMA med_img;
 CREATE TABLE med_img.Instance (
     uuid uuid  NOT NULL DEFAULT uuid_generate_v4(),
     series_uuid uuid  NOT NULL,
-    created timestamp  NOT NULL DEFAULT current_timestamp,
     comment text  NULL,
     original boolean  NOT NULL DEFAULT true,
     image pgcv_core.ndarray_int4  NOT NULL,
+    created_at timestamp  NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp  NOT NULL DEFAULT current_timestamp,
+    active boolean  NOT NULL DEFAULT true,
     CONSTRAINT Instance_pk PRIMARY KEY (uuid)
 );
 
 COMMENT ON TABLE med_img.Instance IS 'This table stores basic instance/image information inspired by the DICOM standard.';
 COMMENT ON COLUMN med_img.Instance.uuid IS 'Universally unique identifier of the instance in the database.';
 COMMENT ON COLUMN med_img.Instance.series_uuid IS 'Reference to the uuid of the series.';
-COMMENT ON COLUMN med_img.Instance.created IS 'Datetime the image pixel data creation started.';
 COMMENT ON COLUMN med_img.Instance.original IS 'Describes whether an image pixel values were based on source data or have been derived in some manner from the pixel value of one or more other images';
 COMMENT ON COLUMN med_img.Instance.image IS 'The datatype containing the image data. Refer to the pgcv PostgreSQL extension.';
+COMMENT ON COLUMN med_img.Instance.created_at IS 'Datetime the image pixel data creation started.';
+COMMENT ON COLUMN med_img.Instance.updated_at IS 'Datetime the image pixel data was last updated';
+COMMENT ON COLUMN med_img.Instance.active IS 'Defines whether an instance is active';
 
 -- Table: Patient
 CREATE TABLE med_img.Patient (
@@ -30,9 +34,12 @@ CREATE TABLE med_img.Patient (
     family_name varchar(35)  NOT NULL,
     other_ids jsonb  NULL,
     email varchar(320)  NULL,
-    region jsonb  NOT NULL,
+    address jsonb  NOT NULL,
     sex smallint  NOT NULL,
     birthdate date  NOT NULL,
+    created_at timestamp  NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp  NOT NULL DEFAULT current_timestamp,
+    active boolean  NOT NULL DEFAULT true,
     CONSTRAINT Patient_pk PRIMARY KEY (uuid)
 );
 
@@ -42,62 +49,77 @@ COMMENT ON COLUMN med_img.Patient.given_name IS 'The given name of the person.';
 COMMENT ON COLUMN med_img.Patient.family_name IS 'The family name of the person, usually the last name.';
 COMMENT ON COLUMN med_img.Patient.other_ids IS 'JSON array of alternate identifiers for the patient.';
 COMMENT ON COLUMN med_img.Patient.email IS 'Email address of the patient';
-COMMENT ON COLUMN med_img.Patient.region IS 'JSON object containing information about the region of the patient, for instance, country, state/province, city';
+COMMENT ON COLUMN med_img.Patient.address IS 'JSON object containing information about the region of the patient, for instance, country, state/province, city';
 COMMENT ON COLUMN med_img.Patient.sex IS 'Sex of the patient, according to the ISO/IEC 5218: Codes for the representation of human sexes.';
 COMMENT ON COLUMN med_img.Patient.birthdate IS 'Birthdate of the patient';
+COMMENT ON COLUMN med_img.Patient.created_at IS 'Date and time when the patient was created';
+COMMENT ON COLUMN med_img.Patient.updated_at IS 'Date and time when the patient was last updated';
+COMMENT ON COLUMN med_img.Patient.active IS 'Defines whether the patient is active';
 
 -- Table: Region
 CREATE TABLE med_img.Region (
     instance_uuid uuid  NOT NULL,
-    created timestamp  NOT NULL DEFAULT current_timestamp,
     method varchar(70)  NOT NULL,
     props pgcv_core.regionprops  NOT NULL,
     category varchar(70)  NULL,
+    created_at timestamp  NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp  NOT NULL DEFAULT current_timestamp,
+    active boolean  NOT NULL DEFAULT true,
     CONSTRAINT Region_pk PRIMARY KEY (instance_uuid,method,props)
 );
 
 COMMENT ON TABLE med_img.Region IS 'This table stores information about the regions found in an image using a segmentation method';
 COMMENT ON COLUMN med_img.Region.instance_uuid IS 'Reference to the uuid of the instance.';
-COMMENT ON COLUMN med_img.Region.created IS 'Datetime the region information was extracted from the image';
 COMMENT ON COLUMN med_img.Region.method IS 'Method used to extract the region properties. This allows for different segmentation functions';
 COMMENT ON COLUMN med_img.Region.props IS 'The datatype containing properties of the region. Refer to the pgcv PostgreSQL extension.';
 COMMENT ON COLUMN med_img.Region.category IS 'Class of region according to a classifier';
+COMMENT ON COLUMN med_img.Region.created_at IS 'Datetime the region information was extracted from the image';
+COMMENT ON COLUMN med_img.Region.updated_at IS 'Datetime the region was last updated';
+COMMENT ON COLUMN med_img.Region.active IS 'Defines whether the region is active';
 
 -- Table: Series
 CREATE TABLE med_img.Series (
     uuid uuid  NOT NULL DEFAULT uuid_generate_v4(),
     study_uuid uuid  NOT NULL,
-    created timestamp  NOT NULL DEFAULT current_timestamp,
     laterality char(1)  NULL,
     description varchar(255)  NOT NULL,
     body_part varchar(35)  NOT NULL,
+    created_at timestamp  NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp  NOT NULL DEFAULT current_timestamp,
+    active boolean  NOT NULL DEFAULT true,
     CONSTRAINT Series_pk PRIMARY KEY (uuid)
 );
 
 COMMENT ON TABLE med_img.Series IS 'This table stores basic series information inspired by the DICOM standard.';
 COMMENT ON COLUMN med_img.Series.uuid IS 'Universally unique identifier of the series in the database.';
 COMMENT ON COLUMN med_img.Series.study_uuid IS 'Reference to the uuid of the study.';
-COMMENT ON COLUMN med_img.Series.created IS 'Date and time when the series started';
 COMMENT ON COLUMN med_img.Series.laterality IS 'Laterality of (paired) body part examined. Required if the body part examined is a paired structure. R = right, L = left';
 COMMENT ON COLUMN med_img.Series.description IS 'User provided description of the Series';
 COMMENT ON COLUMN med_img.Series.body_part IS 'Text description of the part of the body examined';
+COMMENT ON COLUMN med_img.Series.created_at IS 'Date and time when the series started';
+COMMENT ON COLUMN med_img.Series.updated_at IS 'Date and time when the series was last updated';
+COMMENT ON COLUMN med_img.Series.active IS 'Defines whether the study is active';
 
 -- Table: Study
 CREATE TABLE med_img.Study (
     uuid uuid  NOT NULL DEFAULT uuid_generate_v4(),
     patient_uuid uuid  NOT NULL,
-    created timestamp  NOT NULL DEFAULT current_timestamp,
     description varchar(255)  NOT NULL,
     summary jsonb  NULL,
+    created_at timestamp  NOT NULL DEFAULT current_timestamp,
+    updated_at timestamp  NOT NULL DEFAULT current_timestamp,
+    active boolean  NOT NULL DEFAULT true,
     CONSTRAINT Study_pk PRIMARY KEY (uuid)
 );
 
 COMMENT ON TABLE med_img.Study IS 'This table stores basic study information inspired by the DICOM standard.';
 COMMENT ON COLUMN med_img.Study.uuid IS 'Universally unique identifier of the study in the database.';
 COMMENT ON COLUMN med_img.Study.patient_uuid IS 'Reference to the uuid of the patient.';
-COMMENT ON COLUMN med_img.Study.created IS 'Date and time when the study started';
 COMMENT ON COLUMN med_img.Study.description IS 'Description or classification of the Study performed.';
 COMMENT ON COLUMN med_img.Study.summary IS 'Stores information about the overall study and reporting information, for instance, the BI-RADS category';
+COMMENT ON COLUMN med_img.Study.created_at IS 'Date and time when the study started';
+COMMENT ON COLUMN med_img.Study.updated_at IS 'Date and time when the study was last updated';
+COMMENT ON COLUMN med_img.Study.active IS 'Defines whether the study is active';
 
 -- foreign keys
 -- Reference: Image_Series (table: Instance)
--- databases/medical_imaging/medical_imaging_test.sql
+++ databases/medical_imaging/medical_imaging_test.sql
@@ -1,23 +1,23 @@
 INSERT INTO
-  med_img.Patient(given_name, family_name, email, region, sex, birthdate)
+  med_img.Patient(uuid, given_name, family_name, email, address, sex, birthdate)
 VALUES
-  ('Maria', 'Hernandez', 'maria@gmail.com', '{"country": "Costa Rica", "province": "Alajuela", "canton": "Grecia"}', 2, '1968-01-08'),
-  ('Petra', 'Martinez', 'petra@gmail.com', '{"country": "Costa Rica", "province": "Heredia", "canton": "San Francisco"}', 2, '1958-04-20');
+  ('d5c717ee-6202-4889-a042-059ecf649c5a', 'Maria', 'Hernandez', 'maria@gmail.com', '{"country": "Costa Rica", "state": "Alajuela", "city": "Grecia"}', 2, '1968-01-08'),
+  ('13909736-3b71-443a-9dc5-64ce885653e3', 'Petra', 'Martinez', 'petra@gmail.com', '{"country": "Costa Rica", "state": "Heredia", "city": "San Francisco"}', 2, '1958-04-20');
 -- SELECT * from med_img.Patient;
--- SELECT * from med_img.Patient where region->>'province' = 'Heredia';
+-- SELECT * from med_img.Patient where address->>'province' = 'Heredia';
 
 INSERT INTO
-  med_img.Study(patient_uuid, description)
+  med_img.Study(uuid, patient_uuid, description)
 VALUES
-  ('d5c717ee-6202-4889-a042-059ecf649c5a', 'Digital Mammogram'),
-  ('13909736-3b71-443a-9dc5-64ce885653e3', 'Digital Mammogram');
+  ('a8e03b9a-3dc6-4c89-af16-4e7d659c724a', 'd5c717ee-6202-4889-a042-059ecf649c5a', 'Digital Mammogram'),
+  ('9fd67171-b5e0-4165-a2a2-348d0a21b0e3', '13909736-3b71-443a-9dc5-64ce885653e3', 'Digital Mammogram');
 -- SELECT * from med_img.Study;
 
 INSERT INTO
-  med_img.Series(study_uuid, laterality, description, body_part)
+  med_img.Series(uuid, study_uuid, laterality, description, body_part)
 VALUES
-  ('a8e03b9a-3dc6-4c89-af16-4e7d659c724a', 'L', 'MLO projections', 'BREAST'),
-  ('9fd67171-b5e0-4165-a2a2-348d0a21b0e3', 'R', 'MLO projections', 'BREAST');
+  ('086c8575-49b8-47b4-82e5-4203a5064835', 'a8e03b9a-3dc6-4c89-af16-4e7d659c724a', 'L', 'MLO projections', 'BREAST'),
+  ('873a975d-4a78-46b6-9516-9ab2be9445c2', '9fd67171-b5e0-4165-a2a2-348d0a21b0e3', 'R', 'MLO projections', 'BREAST');
 -- SELECT * from med_img.Series;
 
 -- WARNING: Don't 'SELECT *' with this table, you dont want to print 1048576 pixel values to the console. Instead do:
@@ -29,7 +29,7 @@ VALUES
 -- SELECT i.uuid, i.series_uuid, i.created,i.comment, i.original, (i.image).shape FROM med_img.Instance i;
 
 INSERT INTO
-  med_img.Region(instance_uuid, created, method, props)
+  med_img.Region(instance_uuid, created_at, method, props)
 SELECT
     i.uuid,
     CURRENT_TIMESTAMP,
